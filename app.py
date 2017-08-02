00fb1dc7ebe75f27f438237a45fa1c4f4617d74d
Jose Thomas Dominic <josethomasd@gmail.com>
2017-08-02 15:30:30 +0530

    test

--- app.py
+++ app.py
@@ -33,21 +33,20 @@ def webhook():
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-    def generate():
-        base_url = "https://graph.facebook.com/v2.8/"
-        access_token = os.environ["PAGE_ACCESS_TOKEN"]
-        data = request.get_json()
-        try:
-            if data["object"] == "page":
-                if data["entry"][0]["messaging"]:
-                    sender_id = data["entry"][0]["messaging"][0]["sender"]["id"]
-                    # the facebook ID of the person sending you the message 
-              
-		    message_data = "Hello there!"
-		    send_message(sender_id, message_data)
-        except:
-		pass
-	return "ok", 200, generate()
+    base_url = "https://graph.facebook.com/v2.8/"
+    access_token = os.environ["PAGE_ACCESS_TOKEN"]
+    data = request.get_json()
+    try:
+        if data["object"] == "page":
+            if data["entry"][0]["messaging"]:
+                sender_id = data["entry"][0]["messaging"][0]["sender"]["id"]
+                # the facebook ID of the person sending you the message 
+                
+                message_data = "Hello there!"
+                send_message(sender_id, message_data)
+    except:
+        pass
+	return "ok", 200
 
 def send_message(recipient_id, message_text):
 
@@ -68,30 +67,7 @@ def send_message(recipient_id, message_text):
         }
     })
 
-    r = requests.post("https://graph.facebook.com/v2.8/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-    return "ok", 200
-
-def send_state(recipient_id):
-
-    log("sending state to {recipient}: ".format(recipient=recipient_id))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "sender_action":"typing_on"
-
-    })
-    r = requests.post("https://graph.facebook.com/v2.8/me/messages", params=params, headers=headers, data=data)
+    r = requests.post("https://graph.facebook.com/v2.9/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
