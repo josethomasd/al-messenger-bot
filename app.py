eeb3558e0a67cdd868096a7cd196dcb05432a311
Jose Thomas Dominic <josethomasd@gmail.com>
2017-08-02 15:47:32 +0530

    test

--- app.py
+++ app.py
@@ -41,14 +41,31 @@ def webhook():
             if data["entry"][0]["messaging"]:
                 sender_id = data["entry"][0]["messaging"][0]["sender"]["id"]
                 # the facebook ID of the person sending you the message 
-                
-                message_data = "Hello there!"
+                message_text = '+'.join(message_text.split(" "))
+                message_data = get_message(message_text)
                 send_message(sender_id, message_data)
     except:
         pass
 
     return "ok", 200
-
+def get_message(incoming_msg):
+    if(incoming_msg.lower()=="what is alula"):
+        message_data = "Alula is the first phone case to hold, protect, and dispense your brith control pills!  You simply put your pills into the case every month and our app will remind you everyday at the optimal time to take them!"
+    elif(incoming_msg.lower()=="is alula protective for my phone?"):
+        message_data = "Absolutely! Alula uses (enter materials here) those same materials are used in some of the best selling phone cases on the market."
+    elif(incoming_msg.lower=="how does it work?"):
+        message_data = "Insert how to video"
+    elif(incoming_msg.lower=="will radiation from the phone affect my pills?"):
+        message_data = "Radiation means heat! Heat would be bad for your pill but luckily we are using (enter material here) that will protect and insulate the pills from your iPhone’s maximum operating temperature. If your phone overheats, we recommend removing the case to keep your pills as safe as possible! "
+    elif(incoming_msg.lower=="can I leave alula in the sun for extended amounts of time?"):
+        message_data = "It is not recommended to leave your phone in the sun for extended amount of time as it acts as a conductor and will cause your phone to overheat. If your phone overheats, we recommend removing the case to keep your pills as safe as possible!"
+    elif(incoming_msg.lower=="how should I load it?"):
+        message_data = "Load it by day as marked on your blister pack! There are letters to represent the day of the week, just match em up!"
+    elif(incoming_msg.lower=="what kind of birth control pills does it hold?"):
+        message_data = "All kinds! Though the case pill plate dimensions are the appropriate depth for the average birth control pill regardless of brand."
+    else:
+        message_data = "Thanks for messaging us! We will get back to you shortly."
+    return message_data
 def send_message(recipient_id, message_text):
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
